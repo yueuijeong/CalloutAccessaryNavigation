1ab5af64a1dd13324ff17257f8d182cad46e5fe2
D7703_21 <d7703_21@d7703-21ui-imac.local>
2017-09-14 14:49:03 +0900

    Initial Commit

---
+++ DITMap/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  DITMap
+//
+//  Created by 김종현 on 2017. 8. 31..
+//  Copyright © 2017년 김종현. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet weak var myMapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //  지도의 center 위치, DIT 위치정보 35.166197, 129.072594
+        let center = CLLocationCoordinate2DMake(35.166197, 129.072594)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(center, span)
+        
+        myMapView.setRegion(region, animated: true)
+        
+        // Annotation(Pin) 꼽기
+        let anno01 = MKPointAnnotation()
+        anno01.coordinate = center
+        anno01.title = "DIT 동의과학대학교"
+        anno01.subtitle = "나의 꿈이 자라는 곳"
+        
+        // 부산시민공원 35.168444, 129.057832
+        let anno02 = MKPointAnnotation()
+        anno02.coordinate.latitude = 35.168444
+        anno02.coordinate.longitude = 129.057832
+        anno02.title = "부산시민공원"
+        anno02.subtitle = "부산시민들의 휴식처"
+        
+        myMapView.addAnnotation(anno01)
+        myMapView.addAnnotation(anno02)
+        
+        myMapView.delegate = self
+    }
+    
+    // MKMapViewDelegate method
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let identifier = "MyPin"
+        var  annotationView = myMapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+            
+        let leftIconView = UIImageView(frame: CGRect(x: 0, y: 0, width: 53, height: 53))
+            
+            if annotation.title! == "부산시민공원" {
+                annotationView?.pinTintColor = UIColor.green
+                leftIconView.image = UIImage(named:"park.png" )
+                annotationView?.leftCalloutAccessoryView = leftIconView
+            }
+            
+            if annotation.title! == "DIT 동의과학대학교" {
+                annotationView?.pinTintColor = UIColor.green
+                leftIconView.image = UIImage(named:"dit.png" )
+                annotationView?.leftCalloutAccessoryView = leftIconView
+            }
+            
+            
+        } else {
+            annotationView?.annotation = annotation
+        }
+        
+       
+        
+        let btn = UIButton(type: .detailDisclosure)
+        annotationView?.rightCalloutAccessoryView = btn
+        
+        return annotationView
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        if control == view.rightCalloutAccessoryView{
+            
+            self.performSegue(withIdentifier: "goDetail", sender: self)
+        }
+        
+//        print("callout Accessory Tapped!")
+//        
+//        let viewAnno = view.annotation
+//        let viewTitle: String = ((viewAnno?.title)!)!
+//        let viewSubTitle: String = ((viewAnno?.subtitle)!)!
+//        
+//        print("\(viewTitle) \(viewSubTitle)")
+        
+//        
+//        let ac = UIAlertController(title: viewTitle, message: viewSubTitle, preferredStyle: .alert)
+//        ac.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+//        ac.addAction(UIAlertAction(title: "DELETE", style: .destructive, handler: nil))
+//
+//        
+//        present(ac, animated: true, completion: nil)
+        
+    }
+
+}
+
